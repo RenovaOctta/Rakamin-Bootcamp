4eb84176902cae4732518145d71c5568432291e8
RenovaOctta <renovainformatika@gmail.com>
2021-08-04 22:20:57 +0700

    First Commit Add index, css dan images - Tugas Homework 1

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Website</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+<body>
+    <section>
+        <!--navigation-->
+        <nav>
+            <a href="" class="text-tugas"> Homework 1 HTML dan CSS Mastery </a>
+        </nav>
+        <!--text left-->
+        <div class="text-left">
+            <p>Assalamu'alaikum Wr.Wb</p>
+            <p>Saya Renova Octaviawati</p>
+            <p>Saya Mengikuti Bootcamp Rakamin Academy</p>
+            <p>Digital Talent Scholarship Kominfo</p>
+        </div>
+        <!--image top-->
+        <img src="images/ren.png" class="image-top" alt="image-top">
+    </section>
+    <!--about-->
+    <div class="about">
+        <div class="about-text">
+            <p>About Me</p>
+            <p>Perkenalkan saya Renova Octaviawati, Lahir di Bandung pada tanggal 16. </p>
+        </div>
+    </div>
+
+</body>
+</html>
