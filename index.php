d85aa89e6efccc5184b10a68b0dc9c2c4aa362bd
RenovaOctta <renovainformatika@gmail.com>
2021-08-05 23:50:24 +0700

    modified style.css dan index.php add about me

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Personal Website</title>
     <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <section>
@@ -18,7 +20,7 @@
             <p>Assalamu'alaikum Wr.Wb</p>
             <p>Saya Renova Octaviawati</p>
             <p>Saya Mengikuti Bootcamp Rakamin Academy</p>
-            <p>Digital Talent Scholarship Kominfo</p>
+            <p>Digital Talent Scholarship - Kominfo</p>
         </div>
         <!--image top-->
         <img src="images/ren.png" class="image-top" alt="image-top">
@@ -27,9 +29,22 @@
     <div class="about">
         <div class="about-text">
             <p>About Me</p>
-            <p>Perkenalkan saya Renova Octaviawati, Lahir di Bandung pada tanggal 16. </p>
+            <p>Perkenalkan saya Renova Octaviawati, anak ketujuh dari delapan bersaudara. Ayah saya telah meninggal tepatnya pada tahun 2020,
+               saat ini tinggal Ibu lah yang saya jaga. Maka untuk kalian yang masih memiliki kedua orangtua lengkap, jagalah ia, berbaktilah dan bahagiakanlah mereka.
+               Sejak kecil saya suka bernyanyi hingga pada kelas VII saya masuk dan aktif dalam Group Vokal Nasyid. Saya juga mengikuti beberapa perlombaan baik itu lomba akademik
+               maupun non-akademik. Seiring berjalannya waktu saya melanjutkan sekolah Perguruan Tinggi di salah satu kampus swasta 
+               di Bandung. Saat kuliah saya sambil bekerja untuk mengurangi beban orangtua. Alhamdulillah pada tahun 2019 saya Lulus,
+               setelah Lulus saya sempat bekerja disalah satu Perguruan Tinggi Negeri sebagai Tenaga Ahli LMS (Learning Management System).
+               Dan saat ini saya mengikuti Digital Talent Scholarship 2021 - Kominfo khususnya di program IT Front End Developer karna saya
+               ingin meningkatkan skill dan knowladge saya lebih lagi, sehingga bisa lebih siap untuk bersaing didunia kerja kedepannya. 
+            </p>
         </div>
     </div>
-
+    <!--social-media-->
+    <div class="social">
+        <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook-f"></i></a>
+        <a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram"></i></a>
+        <a href="https://www.whatsapp.com/" target="_blank"><i class="fa fa-whatsapp"></i></a>
+    </div>
 </body>
 </html>
